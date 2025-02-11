50b7bd0c3d88e097fbfa168b9376df9af9d969ca
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-02-11 14:28:43 +0200

    Add files via upload

---
+++ äänestys sovellus/main.js
@@ -0,0 +1,65 @@
+//Button variables
+const registerBtn=document.getElementById("registerBtn");
+const loginBtn=document.getElementById("loginBtn");
+//registration name and password
+const nimi_registration=document.getElementById("nimi_registration");
+const salasana_registration=document.getElementById("salasana_registration");
+//login name and password
+const nimi_login=document.getElementById("nimi_login");
+const salasana_login=document.getElementById("salasana_login");
+//current user password and login
+let user_sopiva_salasana=undefined;
+let user_sopiva_nimi=undefined;
+//current admin password and login
+let admin_sopiva_salasana=undefined;
+let admin_sopiva_nimi=undefined;
+
+function Register(){
+    if(document.getElementById("user_checkbox").checked===true){
+        user_sopiva_nimi=nimi_registration.value;
+        user_sopiva_salasana=salasana_registration.value;
+        if(user_sopiva_nimi!==undefined &&user_sopiva_salasana!==undefined){
+            window.alert("Rekiströiti onnistui!");
+        }
+    }
+    else if(document.getElementById("admin_checkbox").checked===true){
+        admin_sopiva_nimi=nimi_registration.value;
+        admin_sopiva_salasana=salasana_registration.value;
+        if(admin_sopiva_nimi!==undefined &&admin_sopiva_salasana!==undefined){
+            window.alert("Rekiströiti onnistui!");
+        }
+    }
+}
+
+function Login(){
+    if(document.getElementById("user_checkbox").checked===true){
+        if(nimi_login.value===user_sopiva_nimi && salasana_login.value===user_sopiva_salasana){
+            window.alert("Kirjautuminen onnistui!");
+            window.location.href = "user_page.html";
+        }else{
+            window.alert("Kirjautuminen epäonnistui");
+        }
+    }
+    else if(document.getElementById("admin_checkbox").checked===true){
+        if(nimi_login.value===admin_sopiva_nimi && salasana_login.value===admin_sopiva_salasana){
+            window.alert("Kirjautuminen onnistui!");
+            window.location.href = "admin_page.html";
+        }else{
+            window.alert("Kirjautuminen epäonnistui");
+        }
+    }
+}
+
+function unCheckOther(checked_box){//varmistaa ettei voi valita kaksi vaihtoehtoja samana aikana
+    document.getElementById(checked_box).checked=false;
+}
+
+
+
+
+
+
+document.getElementById("user_checkbox").addEventListener("change", () => unCheckOther("admin_checkbox"));
+document.getElementById("admin_checkbox").addEventListener("change", () => unCheckOther("user_checkbox"));
+registerBtn.addEventListener("click",Register);
+loginBtn.addEventListener("click",Login);
---
+++ äänestys sovellus/usecasekuvaus.md
@@ -0,0 +1,39 @@
+|Käyttötapauksen nimi|Äänestyksen sovellus|
+|:-------:|:------:|
+|Käyttäjät|Käyttäjä(ilman ylläpitäjän oikeuksia), Ylläpitäjä|
+|Laukaisija|Käyttäjä halaua äänestää vai katsoa äänestyksiä|
+|Esiehto|Käyttäjän on kirjauduttava sisään|
+|Jälkiehto|Äänestys on tietokanassa|
+
+
+|Käyttäjän| käyttötapauksen kulku|
+|-------:|:------|
+|1:|Käyttäjä kirjatuu tiliin|
+|2:|Käyttäjä valitse halumansa äänestyys|
+|3:|Käyttäjä äänestää|
+
+|Käyttäjän| käyttötapauksen kulku 2|
+|-------:|:------|
+|1:|Käyttäjä kirjatua tiliin|
+|2:|Käyttäjä katsoo olevia äänestyksä|
+
+|Käyttäjän| Poikkeuksellinen toiminta|
+|-------:|:------|
+|1:|Käyttäjä syöttää väärin salasana tai tili|
+|2:|Järjestyys näyttää virhe|
+
+|Ylläpitäjän| käyttötapauksen kulku|
+|-------:|:------|
+|1:|Ylläpitäjä kirjatuu tiliin|
+|2:|Ylläpitäjä luo uuden äänestyksen|
+
+|Ylläpitäjän| käyttötapauksen kulku 2|
+|-------:|:------|
+|1:|Ylläpitäjä kirjatuu tiliin|
+|2:|Ylläpitäjä valitse äänestys|
+|3:|Ylläpitäjä poistaa äänestystä|
+
+|Ylläpitäjän| Poikkeuksellinen toiminta|
+|-------:|:------|
+|1:|Ylläpitäjän syöttää väärin salasana tai tili|
+|2:|Järjestyys näyttää virhe|
