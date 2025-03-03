6916aed887ec5c24150c57fd553eb90422a7fd5b
Ratchat1918 <160712854+Ratchat1918@users.noreply.github.com>
2025-03-03 11:47:27 +0200

    Update admin.js

--- äänestys sovellus/admin.js
+++ äänestys sovellus/admin.js
@@ -1,14 +1,15 @@
 let current_poll_number = 0;
-let uudet_aanestykset = [];
-let poll_details = {}; 
-import {panel_visiblity} from './main.js';
-
-function Panel_Visible(){
-    if(panel_visiblity===false){
-        document.getElementById("moderator_panel").innerHTML='';
-    }
+let poll_details = {};
+let uudet_aanestykset = [
+];
+if(sessionStorage.getItem("Loged_as_user")===true){
+    document.getElementById("moderator_panel").style.display="block";
 }
-Panel_Visible();
+for(let x=1;x<localStorage.length;x++){
+    uudet_aanestykset.push(localStorage.getItem(`Poll${x}`))
+};
+document.getElementById("poll_list_container_h2").textContent = "Äänestykset";
+document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
 
 function createPoll() {
     current_poll_number += 1;
@@ -22,49 +23,52 @@ function createPoll() {
         votesFor: vote_for,
         votesAgainst: votes_against
     };
-    uudet_aanestykset.push(`<li><a href="#" role="button" id="poll${current_poll_number}_link" onclick="createVoteBtn(${current_poll_number})">${poll_name}</a>
-    <a id="votes_for${current_poll_number}"> Votes for: ${vote_for} |</a><a id="votes_against${current_poll_number}"> Votes against: ${votes_against}</a></li>`);
+    poll_id=current_poll_number;
+    aanestyys=`<li><a href="#" role="button" id="poll${poll_id}_link" onclick="createVoteBtn(${poll_id})">${poll_details[poll_id].nimi}</a>
+    <a id="votes_for${poll_id}"> Votes for: ${poll_details[poll_id].votesFor} |</a><a id="votes_against${poll_id}"> Votes against: ${poll_details[poll_id].votesAgainst}</a></li>`;
+    localStorage.setItem(`Poll${poll_id}`,aanestyys);
+    localStorage.setItem(`Kuvaus${poll_id}`,poll_details[poll_id].kuvaus);
     document.getElementById("poll_list_container_h2").textContent = "Äänestykset";
+    for(let x=1;x<localStorage.length+1;x++){
+        uudet_aanestykset.push(localStorage.getItem(`Poll${x}`))
+    };
     document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
 }
 
 function delete_poll() {
-    let a = 0;
-    let poll_nimi_prompt = prompt("Syötä äänestyksen nimi:");
-    for (let x in poll_details) {
-        if (poll_details[x].nimi === poll_nimi_prompt) {
-            delete poll_details[x];
-            uudet_aanestykset.splice(a, 1);
-            document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
-            break;
-        } else {
-            a += 1;
-        }
-    }
+    let poll_numero_prompt = prompt("Syötä äänestyksen numero:");
+    localStorage.removeItem(`Poll${poll_numero_prompt}`);
+    localStorage.removeItem(`Kuvaus${poll_id}`);
+    uudet_aanestykset = [];
+    for(let x=0;x<localStorage.length;x++){
+        uudet_aanestykset.push(localStorage.getItem(`Poll${x}`))
+    };
+    document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
 }
 
 function createVoteBtn(poll_id) {
-    let poll = poll_details[poll_id];
     document.getElementById("poll_details").innerHTML = "<h2>Äänestyksen yksityiskohdat</h2>";
-    document.getElementById("poll_details").innerHTML += `<p>${poll.kuvaus}</p>`;
-    document.getElementById('poll_buttons').innerHTML = `
-        <button id='Vote_ForBtn' onclick='Vote_For(${poll_id})'>Vote for</button>
-        <button id='Vote_AgainstBtn' onclick='Vote_Against(${poll_id})'>Vote against</button>`;
+    document.getElementById("poll_details").innerHTML += `<p>${localStorage.getItem(`Kuvaus${poll_id}`)}</p>`;
+    document.getElementById('poll_buttons').innerHTML = `<button id='Vote_ForBtn' onclick='Vote_For(${poll_id})'>Vote for</button>
+    <button id='Vote_AgainstBtn' onclick='Vote_Against(${poll_id})'>Vote against</button>`;
 }
 
 function Vote_For(poll_id) {
     poll_details[poll_id].votesFor += 1;
-    uudet_aanestykset[poll_id - 1] = `<li><a href="#" role="button" id="poll${poll_id}_link" onclick="createVoteBtn(${poll_id})">${poll_details[poll_id].nimi}</a>
-        <a id="votes_for${poll_id}"> Votes for: ${poll_details[poll_id].votesFor} |</a><a id="votes_against${poll_id}"> Votes against: ${poll_details[poll_id].votesAgainst}</a></li>`;
+    uudet_aanestykset[poll_id] = `<li><a href="#" role="button" id="poll${poll_id}_link" onclick="createVoteBtn(${poll_id})">${poll_details[poll_id].nimi}</a>
+    <a id="votes_for${poll_id}"> Votes for: ${poll_details[poll_id].votesFor} |</a><a id="votes_against${poll_id}"> Votes against: ${poll_details[poll_id].votesAgainst}</a></li>`;
+    document.getElementById("poll_list").innerHTML ='';
     document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
 }
 
 function Vote_Against(poll_id) {
     poll_details[poll_id].votesAgainst += 1;
-    uudet_aanestykset[poll_id - 1] = `<li><a href="#" role="button" id="poll${poll_id}_link" onclick="createVoteBtn(${poll_id})">${poll_details[poll_id].nimi}</a>
-        <a id="votes_for${poll_id}"> Votes for: ${poll_details[poll_id].votesFor} |</a><a id="votes_against${poll_id}"> Votes against: ${poll_details[poll_id].votesAgainst}</a></li>`;
+    uudet_aanestykset[poll_id] = `<li><a href="#" role="button" id="poll${poll_id}_link" onclick="createVoteBtn(${poll_id})">${poll_details[poll_id].nimi}</a>
+    <a id="votes_for${poll_id}"> Votes for: ${poll_details[poll_id].votesFor} |</a><a id="votes_against${poll_id}"> Votes against: ${poll_details[poll_id].votesAgainst}</a></li>`;
+    document.getElementById("poll_list").innerHTML ='';
     document.getElementById("poll_list").innerHTML = uudet_aanestykset.join('');
 }
 
 document.getElementById("create_poll").addEventListener("click", createPoll);
+document.getElementById("delete_poll").addEventListener("click", delete_poll);
 document.getElementById("delete_poll").addEventListener("click", delete_poll);
